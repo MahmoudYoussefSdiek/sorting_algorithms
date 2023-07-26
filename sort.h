8c8969e176b7c032164d96b039ac1e05dab1ae0e
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2023-07-25 17:51:18 -0700

    Update sort.h header file

--- sort.h
+++ sort.h
@@ -28,7 +28,9 @@ void quick_sort(int *array, size_t size);
 
 void quicksort_recursive(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
